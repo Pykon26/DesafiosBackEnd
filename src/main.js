53741b4d6d54c8bd494ded4a5cd74e1683593a70
Pykon <matutepic@gmail.com>
2023-09-21 18:14:53 -0300

    mas cosas

--- src/main.js
+++ src/main.js
@@ -4,23 +4,27 @@ import { __dirname } from './path.js';
 import path from 'path';
 import { engine } from 'express-handlebars';
 import { Server } from 'socket.io';
-import http from 'http';
 import productRouter from './routes/products.routes.js'
 import cartRouter from './routes/cart.routes.js'
 import mongoose from 'mongoose'
-import { orderModel } from './models/order.models.js'
-import { userModel } from './models/users.models.js'
 import cartModel from './models/carts.models.js'
+
+
 const app = express()
-const PORT = 4000
+const PORT = 4001
+
+
+const server = app.listen(PORT, () => {
+	console.log(`Servidor desde puerto: ${PORT}`);
+	console.log(`http://localhost:${PORT}`);
+});
+const io = new Server(server);
+
+
 
 mongoose.connect(process.env.MONGO_URL)
     .then(async () => {
-        console.log("BDD conectada")
-
-        const resultado = await cartModel.findOne({ _id: "64f7be67ee3d47232d0cd8b5" })
-        console.log(JSON.stringify(resultado))
-  
+        console.log("BDD conectada")  
     })
     .catch((error) => console.log("Error en conexion con MongoDB ATLAS: ", error))
 
@@ -29,11 +33,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
 
-const io = new Server(server);
 
 
 //Middlewares
@@ -49,7 +49,12 @@ app.set('views', path.resolve(__dirname, './views'));
 
 app.use('/static', express.static(path.join(__dirname, '/public')));
 
-
+app.get('/static', (req, res) => {
+	res.render('index', {
+		rutaCSS: 'index',
+		rutaJS: 'index',
+	});
+});
 
 app.get('/static/realtimeproducts', (req, res) => {
 	res.render('realTimeProducts', {
@@ -57,6 +62,30 @@ app.get('/static/realtimeproducts', (req, res) => {
 		rutaJS: 'realTimeProducts',
 	});
 });
+
+
+app.get('/static/products', (req, res) => {
+	res.render('products', {
+		rutaCSS: 'products',
+		rutaJS: 'products',
+	});
+});
+
+
+app.get('/static/carts', (req, res) => {
+	res.render('carts', {
+		rutaCSS: 'carts',
+		rutaJS: 'carts',
+	});
+});
+
+
+app.get('/static/carts/:cid', (req, res) => {
+	const { cid } = req.params;
+	cartId = cid;
+	res.redirect('/static/carts');
+});
+
 
 app.get('*', (req, res) => {
 	res.status(404).send('Error 404');
